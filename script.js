a8e0fd6a12e87cedcb5f1945681bf90523540cc5
Erika Santos <116293419+ErikavbSantos@users.noreply.github.com>
2023-04-17 19:35:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,85 @@ var textInput = document.querySelector("#input-texto");
 var outInput = document.querySelector("#output-area");
 
 function criptografar() {
-    var texto = textInput.value;
+    var str = textInput.value;
+
+    var texto = str.toUpperCase();
+    console.log(texto);
 
     var resultCripto = texto
-    .replace(/a/g, "bg")
-    .replace(/e/g, "da")
-    .replace(/i/g, "ng")
-    .replace(/o/g, "bn")
-    .replace(/u/g, "fb")
+    .replace(/A/g, "⏃")
+    .replace(/B/g, "⏚")
+    .replace(/C/g, "☊")
+    .replace(/D/g, "⎅")
+    .replace(/E/g, "⟒")
 
-    .replace(/A/g, "BG")
-    .replace(/E/g, "DA")
-    .replace(/I/g, "NG")
-    .replace(/O/g, "BN")
-    .replace(/U/g, "FB");
+    .replace(/F/g, "⎎")
+    .replace(/G/g, "☌")
+    .replace(/H/g, "⊑")
+    .replace(/I/g, "⟟")
+    .replace(/J/g, "⟊")
+
+    .replace(/K/g, "☍")
+    .replace(/L/g, "⌰")
+    .replace(/M/g, "⋔")
+    .replace(/N/g, "⋏")
+    .replace(/O/g, "⍜")
+   
+    .replace(/P/g, "⌿")
+    .replace(/Q/g, "⍾")
+    .replace(/R/g, "⍀")
+    .replace(/S/g, "⌇")
+    .replace(/T/g, "⏁")
+   
+    .replace(/U/g, "⎍")
+    .replace(/V/g, "⎐")
+    .replace(/W/g, "⍙")
+    .replace(/X/g, "⌖")
+    .replace(/Y/g, "⊬")
+
+    .replace(/Z/g, "⋉");
 
     document.getElementById('output-area').innerHTML = '<textarea readonly id="resultado">' + resultCripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function descriptografar() {
-    var texto = textInput.value;
+    var str = textInput.value;
+
+    var texto = str.toUpperCase();
+    console.log(texto);
 
     var resultDescripto = texto
-    .replace(/bg/g, "a")
-    .replace(/da/g, "e")
-    .replace(/ng/g, "i")
-    .replace(/bn/g, "o")
-    .replace(/fb/g, "u")
+    .replace(/⏃/g, "A")
+    .replace(/⏚/g, "B")
+    .replace(/☊/g, "C")
+    .replace(/⎅/g, "D")
+    .replace(/⟒/g, "E")
+    
+    .replace(/⎎/g, "F")
+    .replace(/☌/g, "G")
+    .replace(/⊑/g, "H")
+    .replace(/⟟/g, "I")
+    .replace(/⟊/g, "J")
 
-    .replace(/BG/g, "A")
-    .replace(/DA/g, "E")
-    .replace(/NG/g, "I")
-    .replace(/BN/g, "O")
-    .replace(/FB/g, "U");
+    .replace(/☍/g, "K")
+    .replace(/⌰/g, "L")
+    .replace(/⋔/g, "M")
+    .replace(/⋏/g, "N")
+    .replace(/⍜/g, "O")
+   
+    .replace(/⌿/g, "P")
+    .replace(/⍾/g, "Q")
+    .replace(/⍀/g, "R")
+    .replace(/⌇/g, "S")
+    .replace(/⏁/g, "T")
+   
+    .replace(/⎍/g, "U")
+    .replace(/⎐/g, "V")
+    .replace(/⍙/g, "W")
+    .replace(/⌖/g, "X")
+    .replace(/⊬/g, "Y")
+
+    .replace(/⋉/g, "Z");
 
     document.getElementById('output-area').innerHTML = '<textarea readonly id="resultado">' + resultDescripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 
